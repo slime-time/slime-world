910fe0dcddc1137d8e47e1914df6a20db5cc406d
script-mouse <brygidarichcreek@gmail.com>
2026-02-19 16:05:31 -0500

    Reads Penny's speed and jump velocity from an external file
    Also renamed some files to match agreed-upon naming conventions

--- penny/Penny.gd
+++ penny/Penny.gd
@@ -1,12 +1,19 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var movement_speed = 300.0
+var jump_velocity = -400.0
 var screen
 
+var config = ConfigFile.new()
 func _ready() -> void:
 	screen = get_viewport_rect().size
+	
+	# Attempt to read movement settings from an external file
+	var error = config.load("res://settings.cfg")
+	if(error == OK):
+		movement_speed = config.get_value("penny_movement", "movement_speed")
+		jump_velocity = config.get_value("penny_movement", "jump_velocity")
 
 func _physics_process(delta: float) -> void:
 	# Check if player is OOB, and reset to origin if so
@@ -19,14 +26,15 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_velocity
+	
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * movement_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, movement_speed)
 
 	move_and_slide()
