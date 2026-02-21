6bd8e8b081b543a6f5e27adfaec49e9d572316df
Benjamin Richcreek <richcreekbenjamin@gmail.com>
2026-02-20 20:39:38 -0500

    Implemented Pause functionality
    To pause, press q. To unpause, press q. Also moved some code around to align our progress with what was decided upon in the architecture meeting

---
+++ SceneManager.gd
@@ -0,0 +1,24 @@
+extends Node
+
+# True only when the player is in the pause menu, not during cutscenes or other situations
+var is_paused = false
+
+# True only when the player could pause the game
+var can_pause = true
+
+# When false, _physics_process and _process will do nothing
+var physics_applies = true
+
+# Pause the game, and stop the player from moving. The player can unpause from this position
+func pauseGame():
+	# ToDo: show settings / pause menu UI
+	is_paused = true
+	physics_applies = false
+	InputManager.haltPlayerMovement()
+	
+func unpauseGame():
+	# ToDo: hide settings / pause menu UI
+	is_paused = false
+	InputManager.resumePlayerMovement()
+	physics_applies = true
+	
