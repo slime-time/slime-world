196e9bfea3d172ae62ff592aa0ba4c793c3a2939
Benjamin Richcreek <richcreekbenjamin@gmail.com>
2026-03-06 11:14:30 -0500

    Spikes fall when player walks into spike hitbox

--- Transformers/Spike/Spike.gd
+++ Transformers/Spike/Spike.gd
@@ -1,8 +1,11 @@
-extends CollisionPolygon2D
+extends RigidBody2D
 
 func _ready():
-	set_process_mode(Node.PROCESS_MODE_DISABLED)
+	set_freeze_enabled(true)
+	set_contact_monitor(true)
+	set_lock_rotation_enabled(true)
 	
-	
+# We don't care how many times the spike is told to fall because after being called once,
+# further calls to fall do nothing
 func fall():
-	set_process_mode(Node.PROCESS_MODE_INHERIT)
+	call_deferred("set_freeze_enabled", false)
