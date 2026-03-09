eb6d2e843db670605ccc7451eae7f9f0e5d80c92
enwask <enwask@ucf.edu>
2026-03-09 15:12:03 -0400

    Better fluid flow rendering

--- components/FluidFlow/FluidFlowPart.gd
+++ components/FluidFlow/FluidFlowPart.gd
@@ -1,7 +1,7 @@
 extends Node2D
 class_name FluidFlowPart
 
-@onready var mask_texture: GradientTexture2D = $Mask.texture as GradientTexture2D
+@onready var mask: TextureRect = $Mask
 @onready var flow_control: Control = $Mask/FlowControl
 @onready var main_control: Control = $Mask/FlowControl/MainFlowControl
 @onready var left_flow_part: AnimatedSprite2D = $Mask/FlowControl/LeftFlowPart
@@ -21,14 +21,18 @@ func _ready() -> void:
 
 # Resizes the flow part to the new width
 func init_flow_part(type: FluidFlow.Type, x_offset: int, size: Vector2, clip_start: int = 16, clip_end: int = 16) -> void:
+	if clip_start < 16:
+		# We're using the gradient mask; make it unique
+		mask.texture = mask.texture.duplicate() as GradientTexture2D
+
 	fluid_type = type
 	flow_control.set_size(size + Vector2(4, 0))
 	main_control.set_size(size)
 	main_flow_part.position.x = -x_offset
 	right_flow_part.position.x = main_control.position.x + main_control.size.x
 
-	mask_texture.fill_from.y = clip_start / 16.0
-	mask_texture.fill_to.y = (clip_end + 1) / 16.0
+	mask.texture.fill_from.y = clip_start / 16.0
+	mask.texture.fill_to.y = (clip_end + 1) / 16.0
 
 	# Find a time to resume from so that animations are consistent across reinstancing
 	var time = Time.get_ticks_msec() / 1000.0;
