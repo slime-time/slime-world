31d20604178cdf49abb01ae790227445f3d18d8c
aga <calkowska.aga@gmail.com>
2026-03-10 13:22:50 -0400

    Added GameManager and expanded LevelSelectScreen

--- levels/LevelSelect/LevelSelect.gd
+++ levels/LevelSelect/LevelSelect.gd
@@ -3,14 +3,20 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	# Add level buttons
+	for level_idx in range(1, GameManager.levels.size()):
+		var button = Button.new()
+		
+		button.custom_minimum_size = Vector2(100, 50)
+		button.text = "Level " + str(level_idx)
 
+		# Disable button if level hasn't been unlocked yet
+		if (level_idx > GameManager.highest_level_unlocked):
+			button.disabled = true;
 
+		button.pressed.connect(func(): GameManager.load_level(level_idx))
+		add_child(button)	
+		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_level_select_button_pressed() -> void:
-	print("Button was clicked!")
-	get_tree().change_scene_to_file("res://levels/TestLevel.tscn")
