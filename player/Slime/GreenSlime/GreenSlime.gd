04caeded428e68702d190530e5f3a7e92d3261ff
enwask <enwask@ucf.edu>
2026-03-10 16:20:01 -0400

    Add ice slime

--- player/Slime/GreenSlime/GreenSlime.gd
+++ player/Slime/GreenSlime/GreenSlime.gd
@@ -1,5 +1,5 @@
 
-extends "res://player/Slime/Slime.gd"
+extends Slime
 
 func canBecomePenny():
 	return size >= 8
@@ -8,9 +8,9 @@ func _ready():
 	hitbox_name = "GreenSlime"
 	sprite_name = "slime"
 	movement_name = "green_slime_movement"
+	slime_type = Slime.Type.GREEN_SLIME
 	super()
 	health = config.get_value("slime_health", "green_slime")
-	slime_type = Slime.Type.GREEN_SLIME
 
 # Get the movement ability of this specific slime (including size in calculation)
 func getMovementAbility():
