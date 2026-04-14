7ece5f49da5dc5c88808f170807ee272056258c2
Lex <lexmatthews_@outlook.com>
2026-04-14 16:40:48 -0400

    creating line of sight cone programatically and implemented simple player detection

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -6,8 +6,9 @@ extends CharacterBody2D
 var patrol_index: int
 var walk_speed: float
 
+var los_area : Area2D
 var los_cone_deg: float
-var los_distance: float
+var los_distance: int
 
 # 0:neutral  1:combat
 var combat_state: bool
@@ -18,12 +19,32 @@ var is_patroling: bool
 func ready():
 	combat_state= false
 	read_enemy_data("generic_enemy_parameters")
+	los_area = get_node("SkeletonLoS")
 
 	#establish if enemy will patrol or stay put
 	if (patrol.size() > 1):
 		patrol_index = 1
 		is_patroling = true
 	else: is_patroling = false
+	
+func _physics_process(delta : float):
+	var detected_bodies : Array[Node2D] = los_area.get_overlapping_bodies()
+	for body in detected_bodies:
+		if (body.name == "Penny") or (body.name.contains("Slime")):
+			combat_state = true
+			is_patroling = false
+	if is_patroling:
+		if position
+		return
+	elif combat_state:
+		attack()
+		combat_state = false
+		is_patroling = true
+	
+	return
+	
+func _on_body_entered():
+	return
 	
 func read_enemy_data(sectionName):
 	var config = ConfigFile.new()
@@ -36,6 +57,14 @@ func read_enemy_data(sectionName):
 	walk_speed = config.get_value(sectionName, "walk_speed", walk_speed)
 	los_cone_deg = config.get_value(sectionName, "los_cone_deg", los_cone_deg)
 	los_distance = config.get_value(sectionName, "los_distance", los_distance)
+
+func set_los_cone():
+	#draw polygon for enemy line of sight, can refactor to accept args instead of reading value from settings.cfg
+	var cone = CollisionPolygon2D.new()
+	var y_offset : int = int(tan(los_cone_deg * (PI/180)) * los_distance)
+	cone.polygon= PackedVector2Array([Vector2(0,0), Vector2(los_distance, y_offset), Vector2(los_distance, -y_offset)])
+	
+	los_area.add_child(cone)
 
 @abstract 
 func attack()
