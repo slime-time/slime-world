5c0de95d33a9c94c3fdde797145f15837432266a
aga <calkowska.aga@gmail.com>
2026-04-22 16:20:34 -0400

    Fixed path to main menu

--- GameManager/GameManager.gd
+++ GameManager/GameManager.gd
@@ -4,7 +4,7 @@ var cur_level: int = 0 # Level 0 is the level select screen
 var highest_level_unlocked: int = 1
 
 const levels: Array[String] = [
-	"res://levels/LevelSelect/LevelSelectScreen.tscn",
+	"res://levels/MainMenu/MainMenu.tscn",
 	"res://levels/Tutorial1.tscn",
 	"res://levels/Level1.tscn",
 	"res://levels/Level2.tscn"
@@ -15,7 +15,7 @@ func loadLevel(x : int):
 	cur_level = x
 	get_tree().call_deferred("change_scene_to_file", levels[x])
 
-# Completes the current level we're on and loads next level if one was just unlocked
+## Completes the current level we're on and loads next level if one was just unlocked
 func completeLevel():
 	var nxt : int = cur_level
 
