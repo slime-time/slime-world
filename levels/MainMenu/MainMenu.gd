bb1515fc26d00e58a644de0067cb8a04fb592bd7
aga <calkowska.aga@gmail.com>
2026-04-22 16:39:09 -0400

    code better

--- levels/MainMenu/MainMenu.gd
+++ levels/MainMenu/MainMenu.gd
@@ -6,26 +6,33 @@ extends CanvasLayer
 @onready var LevelSelect = %LevelSelect
 @onready var SaveSlotSelect = %SaveSlotSelect
 
+enum MainMenuState {
+	MAIN_MENU_HOME,
+	NAME_INPUT,
+	LEVEL_SELECT,
+	SAVE_SLOT_SELECT
+}
+
+var mainMenuState: MainMenuState
+
+func _switchToMenuState(newState: MainMenuState):
+	newGameButton.visible = (newState == MainMenuState.MAIN_MENU_HOME)
+	loadGameButton.visible = (newState == MainMenuState.MAIN_MENU_HOME)
+	backButton.visible = (newState != MainMenuState.MAIN_MENU_HOME)
+	LevelSelect.visible = (newState == MainMenuState.LEVEL_SELECT)
+	SaveSlotSelect.visible = (newState == MainMenuState.SAVE_SLOT_SELECT)
+	
+	mainMenuState = newState
+
+func _ready() -> void:
+	_switchToMenuState(MainMenuState.MAIN_MENU_HOME)
+
 func _onNewGamePressed() -> void:
-	newGameButton.visible = false
-	loadGameButton.visible = false
-
-	LevelSelect.visible = true
-	backButton.visible = true
-
+	_switchToMenuState(MainMenuState.LEVEL_SELECT)
 
 func _onLoadGamePressed() -> void:
-	newGameButton.visible = false
-	loadGameButton.visible = false
+	_switchToMenuState(MainMenuState.SAVE_SLOT_SELECT)
 
-	SaveSlotSelect.visible = true
-	backButton.visible = true
-
-## Resets the menu to its default state. Silly? Yes. Works? Also yes.
+## Always returns to main menu. But should be updateable if need be.
 func _onBackButtonPressed() -> void:
-	newGameButton.visible = true
-	loadGameButton.visible = true
-
-	backButton.visible = false
-	LevelSelect.visible = false
-	SaveSlotSelect.visible = false
+	_switchToMenuState(MainMenuState.MAIN_MENU_HOME)
