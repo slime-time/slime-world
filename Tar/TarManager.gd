56bd5cbceb59c96778dec088a5f2ee11795d9445
enwask <enwask@ucf.edu>
2026-04-27 16:08:06 -0400

    Switch to tilemap materials for tar rendering

--- Tar/TarManager.gd
+++ Tar/TarManager.gd
@@ -1,14 +1,16 @@
-extends Node
+extends Node2D
 @warning_ignore_start("integer_division")
 
-const TAR_LAYER_SCENE = preload("res://components/TarLayer/TarLayer.tscn")
-var tar_layer: TarLayer = null
+# const TAR_LAYER_SCENE = preload("res://components/TarLayer/TarLayer.tscn")
+const TAR_LAYER_MATERIAL = preload("res://assets/materials/tar_layer.tres")
 
-# Each bit is 1 if there is a tar glob at the cooresponding location, otherwise 0
-var tar_globs: PackedByteArray
+const GLOB_SIZE: int = 4				# Resolution of the tar grid
+const MAX_WALL_DIST: int = 8			# Max distance from the wall to write to the tar buffer for a given pixel
 
-
-const GLOB_SIZE: int = 4
+var tar_globs: PackedByteArray			# Each bit is 1 if there is a tar glob at the cooresponding location, otherwise 0
+var tar_buffer: Image					# Buffer with tar pixel data for rendering tar layers
+var tar_buffer_tex: ImageTexture		# Tar buffer texture provided to the shader for tar layers
+var _is_buffer_dirty: bool = false		# Whether the tar buffer has been updated this frame
 
 var window_height: int
 var window_width: int
@@ -41,9 +43,18 @@ func _onSceneLoaded(node : Node2D) -> void:
 
 # Runs when a newly loaded scene becomes ready
 func _onSceneReady() -> void:
-	# Instantiate the tar layer and add it to the scene
-	tar_layer = TAR_LAYER_SCENE.instantiate()
-	get_tree().get_current_scene().add_child(tar_layer)
+	# Initialize the tar buffer
+	var viewport_size = get_viewport_rect().size
+	tar_buffer = Image.create(int(viewport_size.x), int(viewport_size.y), false, Image.FORMAT_RGBA8)
+	tar_buffer.fill(Color(0, 0, 0, 0))
+	tar_buffer_tex = ImageTexture.create_from_image(tar_buffer)
+
+	# Instantiate a tar layer for each tilemap layer
+	var tilemaps = get_tree().get_current_scene().find_children("*", "TileMapLayer")
+	for tilemap in tilemaps:
+		var mat = TAR_LAYER_MATERIAL.duplicate()
+		mat.set_shader_parameter("tar_buffer", tar_buffer_tex)
+		tilemap.material = mat
 
 # Get the coordinates that the slime glob object should be placed at
 func convertToCoordinates(x: float, y: float) -> Vector2i:
@@ -74,3 +85,47 @@ func setLocation(true_index: int):
 
 func resetTar():
 	tar_globs.fill(0)
+
+
+# Updates the value at a single pixel in the tar buffer
+func _setBufferPixel(x: int, y: int, right_wall_dist: int, left_wall_dist: int) -> void:
+	# FIXME: min distances with existing values
+	var r = 1.0 - float(right_wall_dist) / float(MAX_WALL_DIST)
+	var g = 1.0 - float(left_wall_dist) / float(MAX_WALL_DIST)
+	tar_buffer.set_pixel(x, y, Color(r, g, 0, 1))
+
+# Sets a glob for the given direction at the given position (in glob coordinates i.e. pixel/4)
+func setBufferGlob(glob_position: Vector2i, wall_normal: Vector2):
+	if wall_normal.x > 0:
+		# Right-facing wall (red channel)
+		for y in range(glob_position.y, glob_position.y + TarManager.GLOB_SIZE):
+			for d in range(0, MAX_WALL_DIST):
+				var x = glob_position.x - TarManager.GLOB_SIZE - 1 - d
+				if x < 0: break
+
+				_setBufferPixel(x, y, d, MAX_WALL_DIST)
+
+		# Mark the buffer dirty for an update at the end of the frame
+		_is_buffer_dirty = true
+
+	elif wall_normal.x < 0:
+		# Left-facing wall (green channel)
+		for y in range(glob_position.y, glob_position.y + TarManager.GLOB_SIZE):
+			for d in range(0, MAX_WALL_DIST):
+				var x = glob_position.x + d
+				if x >= tar_buffer.get_width(): break
+
+				_setBufferPixel(x, y, MAX_WALL_DIST, d)
+
+		# Mark the buffer dirty for an update at the end of the frame
+		_is_buffer_dirty = true
+
+# Commits the current tar buffer to the imagetexture, called after a batch of updates to the tar buffer is done
+func _commitBuffer() -> void:
+	tar_buffer_tex.update(tar_buffer)
+
+func _process(_delta: float) -> void:
+	# If the buffer is dirty, commit our updates to the image texture
+	if _is_buffer_dirty:
+		_commitBuffer()
+		_is_buffer_dirty = false
