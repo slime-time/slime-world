fe7e2874537afafaec25c977dd2980d7d52f16b6
Lex Matthews <tanukiguernica@gmail.com>
2026-04-29 19:58:22 -0400

    she moove

--- player/Penny/Penny.gd
+++ player/Penny/Penny.gd
@@ -2,27 +2,40 @@ class_name Penny
 extends "res://player/PlayerMovement/PlayerMovement.gd"
 
 var attack_hitbox : Area2D	
+var sprite : AnimatedSprite2D
 	
 func _ready():
 	super()
 	read_movement_data("penny_movement")
 	attack_hitbox = get_node("AttackHitbox")
+	sprite = get_node("PennySprite")
+	sprite.animation_changed.connect(sprite.play)
+	sprite.frame_changed.connect(set_hitbox_state)
+	sprite.animation_finished.connect(func(): sprite.set_animation("walk"))
+	sprite.set_animation("walk")
 	attack_hitbox.body_entered.connect(deal_damage)
 	InputManager.penny_attack.connect(attack)
+
+func _process(delta: float) -> void:
+	if !is_on_floor():
+		sprite.stop()
+	else:
+		sprite.play()
 
 # If Penny is hit, she takes 1 damage
 func hit():
 	takeDamage(1)
 	
 func attack():
-	#play animation by changing it
-	set_hitbox_state()
+	sprite.set_animation("attack")
 	
 
 func set_hitbox_state():
-	#temp logic, will use same conditionals as similar function in MeleeSkeleton.gd/SpecialSkeleton.gd
-	attack_hitbox.set_visible(!attack_hitbox.visible)
-	attack_hitbox.set_monitoring(!attack_hitbox.monitoring)
+	if(sprite.animation == "attack"):
+		if(sprite.frame == 3):
+			attack_hitbox.set_monitoring(true)
+		if(sprite.frame == 6):
+			attack_hitbox.set_monitoring(false)
 
 func deal_damage(target : Node2D):
 	if (target is MeleeSkeleton) or (target is RangedSkeleton) or (target is SpecialSkeleton):
