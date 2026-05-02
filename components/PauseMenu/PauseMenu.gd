d6225cc3a7f2c221a6143b50382266a8e4786ed6
aga <calkowska.aga@gmail.com>
2026-05-01 21:32:47 -0400

    Pause menu almost work

---
+++ components/PauseMenu/PauseMenu.gd
@@ -0,0 +1,32 @@
+extends CanvasLayer
+
+func _ready() -> void:
+	hide();
+
+	SceneManager.pause_state_changed.connect(onPauseStateChanged)
+
+## Hides / shows the pause menu on signal from Scence Manager
+func onPauseStateChanged(is_now_paused : bool) -> void:
+	visible = is_now_paused
+
+## It does what you think it does
+func onResumePressed() -> void:
+	print("resuming...")
+	SceneManager.unpauseGame()
+
+## It does what you think it does
+func onResetPressed() -> void:
+	print("resetting...")
+	SceneManager.unpauseGame()
+	SceneManager.resetScene()
+
+## Unlocks and goes to next level
+func onSkipLevelPressed() -> void:
+	print("skipping...")
+	SceneManager.unpauseGame()
+	GameManager.completeLevel()
+
+## Goes back to main menu
+func onBackToMenuPressed() -> void:
+	SceneManager.unpauseGame()
+	GameManager.loadLevel(0)
