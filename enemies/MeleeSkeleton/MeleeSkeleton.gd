f01f26764a8bcf62772f2b69aaf07a5ded87af26
enwask <enwask@ucf.edu>
2026-05-02 01:24:02 -0400

    Fix melee skeleton

--- enemies/MeleeSkeleton/MeleeSkeleton.gd
+++ enemies/MeleeSkeleton/MeleeSkeleton.gd
@@ -8,7 +8,7 @@ var players_hittable: Array[Node2D]
 
 var physics_delta: float
 
-# True only when the skeleton hasn't hit anything during the current animation	
+# True only when the skeleton hasn't hit anything during the current animation
 var can_hit: bool
 func _physics_process(delta: float):
 	physics_delta = delta
@@ -22,7 +22,7 @@ func _ready():
 		can_hit = true
 	)
 	sprite.set_animation("idle")
-	
+
 	attack_trigger = get_node("AttackTrigger")
 	attack_hitbox = get_node("AttackHitbox")
 	attack_trigger.body_entered.connect(targetForAttack)
@@ -33,24 +33,24 @@ func _ready():
 func targetForAttack(target: Node2D):
 	if(target is PlayerMovement):
 		players_hittable.append(target)
-		
+
 func untargetForAttack(target: Node2D):
 	if(target in players_hittable):
 		players_hittable.erase(target)
-		
+
 func read_melee_data(_sectionName):
 	var config = ConfigFile.new()
 	# Attempt to read melee enemy settings from an external file
 	var error = config.load("res://settings.cfg")
 	# Assert that the data was read
 	assert(error == OK, "Failed to read enemy settings from settings.cfg")
-	
+
 	return
 
 func attack(_target = null):
 	#play animation and set hitboxes to active through animation_changed/frame_changed signals
 	if combat_state:
-		#stop 
+		#stop
 		velocity.x = 0
 		sprite.set_animation("attack")
 		#sprite.frame = 0
@@ -63,9 +63,9 @@ func attack(_target = null):
 			if(sprite.animation == "attack"):
 				sprite.set_animation("idle")
 		, CONNECT_ONE_SHOT)
-	
+
 	#wait again so that the player can exploit a whiff or position enemies intentionally
-	
+
 func hit():
 	super()
 	var chunk1 = get_node("AttackHitbox/Chunk1")
@@ -74,13 +74,13 @@ func hit():
 	attack_hitbox.set_visible(false)
 	chunk1.set_deferred("disabled", true)
 	chunk2.set_deferred("disabled", true)
-	
+
 func turnaround():
 	if !sprite.flip_h:
 		sprite.flip_h = true
 	else:
 		sprite.flip_h = false
-		
+
 	los_area.scale.x = -los_area.scale.x
 	attack_trigger.position.x = -attack_trigger.position.x
 	attack_hitbox.position.x = -attack_hitbox.position.x
@@ -98,7 +98,7 @@ func combat_behavior():
 				velocity.x = move_toward(velocity.x, walk_speed, (1000 * physics_delta) / 16)
 	elif(stop_timer.is_stopped()):
 		attack()
-	
+
 func throw_water(target : Node2D):
 	if (target is PlayerMovement) and combat_state and can_hit:
 		var bodies = attack_trigger.get_overlapping_bodies()
@@ -119,3 +119,11 @@ func set_hitbox_state():
 		return
 	if (get_tree() != null and not get_tree().process_frame.is_connected(set_hitbox_state) and sprite.animation == "attack"):
 		get_tree().process_frame.connect(set_hitbox_state, CONNECT_ONE_SHOT)
+
+func reanimate():
+	super()
+	can_hit = true
+	var chunk1 = get_node("AttackHitbox/Chunk1")
+	var chunk2 = get_node("AttackHitbox/Chunk2")
+	chunk1.set_deferred("disabled", false)
+	chunk2.set_deferred("disabled", false)
